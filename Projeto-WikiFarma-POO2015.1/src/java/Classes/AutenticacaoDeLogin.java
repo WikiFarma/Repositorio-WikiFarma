112cbc0ce0cc4c066960413b33f4b7d6d93baf03
Marco <Marco@NOTEMARCO>
2015-05-01 18:34:20 -0300

    Alterações para fazer a conexão

--- Projeto-WikiFarma-POO2015.1/src/java/Classes/AutenticacaoDeLogin.java
+++ Projeto-WikiFarma-POO2015.1/src/java/Classes/AutenticacaoDeLogin.java
@@ -13,57 +13,56 @@ import java.util.*;
 import java.sql.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+
 /**
  *
  * @author Thayse
  */
 public class AutenticacaoDeLogin extends HttpServlet {
-    
-    
+
     private ServletConfig config;
-    
+
     public void init(ServletConfig config)
-    throws ServletException{
-    this.config=config;
-    
-}
+            throws ServletException {
+        this.config = config;
 
-public void doPost(HttpServletRequest request, HttpServletResponse response)
-throws ServletException,IOException{
+    }
 
-PrintWriter out = response.getWriter();
-String connectionURL = "jdbc:mysql://localhost:3306/farmaciadb";
-Connection connection=null;
-ResultSet rs;
-String userName=new String("");
-String passwrd=new String("");
-response.setContentType("text/html");
-try {
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+        String connectionURL = "jdbc:mysql://localhost:3306/farmaciadb";
+        Connection connection = null;
+        ResultSet rs;
+        String nome = new String("");
+        String senha = new String("");                
+        response.setContentType("text/html");
+        try {
 // Load the database driver
-Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
 // Get a Connection to the database
-connection = DriverManager.getConnection(connectionURL, "root", "root"); 
+            connection = DriverManager.getConnection(connectionURL, "root", "");
 //Add the data into the database
-String sql = "select user,password from User";
-Statement s = connection.createStatement();
-s.executeQuery (sql);
-rs = s.getResultSet();
-while (rs.next ()){
-userName=rs.getString("user");
-passwrd=rs.getString("password");
-}
-rs.close ();
-s.close ();
-}catch(Exception e){
-System.out.println("Exception is ;"+e);
-}
-if(userName.equals(request.getParameter("user"))
-&& passwrd.equals(request.getParameter("pass"))){
-out.println("WELCOME "+userName);
-}
-else{
-out.println("Please enter correct username and password");
-out.println("<a href='AutenticacaoLogin.jsp'><br>Login again</a>");
-}
-}
+            String sql = "select nome_usu,senha_usu from usuario";
+            Statement s = connection.createStatement();
+            s.executeQuery(sql);
+            rs = s.getResultSet();
+            while (rs.next()) {                                
+                nome = rs.getString("nome_usu");
+                senha = rs.getString("senha_usu");                
+            }
+            rs.close();
+            s.close();
+        } catch (Exception e) {
+            System.out.println("Exception is ;" + e);
+        }
+        if (nome.equals(request.getParameter("user"))
+                && senha.equals(request.getParameter("pass"))) {
+            out.println("WELCOME " + nome);
+        } else {
+            out.println("POR FAVOR ENTRE COM O LOGIN E A SENHA CORRETA");
+            out.println("<a href='AutenticacaoLogin.jsp'><br>VOLTAR PARA LOGIN</a>");
+        }
+    }
 }
